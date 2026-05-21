201658fb1ba6cc3b8bce51187c3fb778476ee449
Mike <a@a.pl>
2026-05-21 22:13:56 +0200

    UserTask

--- src/main/java/org/process/processorchestratormvn/process/StartProcessController.java
+++ src/main/java/org/process/processorchestratormvn/process/StartProcessController.java
@@ -3,10 +3,14 @@ package org.process.processorchestratormvn.process;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.eximeebpms.bpm.engine.RuntimeService;
+import org.eximeebpms.bpm.engine.TaskService;
 import org.eximeebpms.bpm.engine.runtime.ProcessInstance;
+import org.eximeebpms.bpm.engine.task.Task;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -14,14 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 class StartProcessController {
 
     private final RuntimeService runtimeService;
+    private final TaskService taskService;
 
     @PostMapping("/start")
     public String start() {
 
         ProcessInstance instance =
                 runtimeService.startProcessInstanceByKey(
-                        "process-1"
+                        "process-1", "businessKey", Map.of("cifKi", "5443111123")
                 );
+
         log.info("Starting process {}", instance.getProcessInstanceId());
         return instance.getProcessInstanceId();
     }
@@ -34,9 +40,13 @@ class StartProcessController {
     public void submit(
             @RequestBody FormResultDto body) {
         log.info("Getting company-data form result for {}", body.processInstanceId());
-        runtimeService.createMessageCorrelation("FORM_COMPLETED")
+        Task task = taskService.createTaskQuery()
                 .processInstanceId(body.processInstanceId())
-                .setVariable("rejected", body.rejected())
-                .correlate();
+                .singleResult();
+        taskService.complete(task.getId(), Map.of("rejected", body.rejected()));
+//        runtimeService.createMessageCorrelation("FORM_COMPLETED")
+//                .processInstanceId(body.processInstanceId())
+//                .setVariable("rejected", body.rejected())
+//                .correlate();
     }
 }
--- src/main/java/org/process/processorchestratormvn/process/UserTaskListener.java
+++ src/main/java/org/process/processorchestratormvn/process/UserTaskListener.java
@@ -2,17 +2,23 @@ package org.process.processorchestratormvn.process;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.eximeebpms.bpm.engine.TaskService;
 import org.eximeebpms.bpm.engine.delegate.DelegateTask;
 import org.eximeebpms.bpm.engine.delegate.TaskListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
-class ProcessTaskListener implements TaskListener {
+class UserTaskListener implements TaskListener {
+
+    private final TaskService taskService;
 
     @Override
     public void notify(DelegateTask delegateTask) {
         log.info("Listing eventName={} for id={}", delegateTask.getEventName(), delegateTask.getProcessInstanceId());
+        taskService.complete(delegateTask.getId(), Map.of("rejected", true));
     }
 }
